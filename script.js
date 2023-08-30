a224a3006ce72d115f041f2da5459fe03852ceaf
Raghavendra M <137168147+RaghuCodeWizard@users.noreply.github.com>
2023-08-30 10:51:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your code here
+cy.visit(baseUrl + "/main.html");
+
+// Select the video element
+cy.get('video').should('have.attr', 'controls');
+
+// Select the source element and assert its src attribute
+cy.get('video source').should('have.attr', 'src', 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4');
+
+cy.visit(baseUrl + "/main.html");
+
+// Select the audio element
+cy.get('audio').should('have.attr', 'controls');
+
+// Select the source element and assert its src attribute
+cy.get('audio source').should('have.attr', 'src', 'http://commondatastorage.googleapis.com/codeskulptor-assets/Epoq-Lepidoptera.ogg');
